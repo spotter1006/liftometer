6a192da28026923cb24b76151fb5d7a63770caf9
spotter1006 <scott@taftinstruments.com>
2023-11-05 16:51:06 -0500

    Live print out for debug purposes

--- liftometer.cpp
+++ liftometer.cpp
@@ -62,7 +62,7 @@ int main(){
             int nTotalMs = nSamples * (SAMPLE_RATE_MS); 
             int nMinutes = nTotalMs / 60000;
             double dSeconds = std::fmod((nTotalMs / 1000.0), 60.0);
-            printf("\033[A\33[2K\rAverage(%02d:%2.3f): Accel: %d, Heading: %d, roll: %d pitch: %d", 
+            printf("\033[A\33[2K\rAverage(%02d:%2.3f): Average Heading: %d, Heading: %d, roll: %d pitch: %d", 
             nMinutes, dSeconds, nOffVals[3], nOffVals[2], nOffVals[0], nOffVals[1]);
         }
     }
