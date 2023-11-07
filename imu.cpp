5612979db1e90563c9e2547469e7258e496f8e11
spotter1006 <scott@taftinstruments.com>
2023-11-07 16:23:10 -0500

    non-working crap

--- imu.cpp
+++ imu.cpp
@@ -14,24 +14,44 @@ using namespace std;
 
 extern "C" struct bno055_t bno055;
 extern gpiod::chip chip;
-timed_mutex mtxData;
 
 Imu::Imu(int nBufferSize){
     m_bKeepRunning = true;
     m_nBufferSize =nBufferSize;
     m_pData = new list<ImuData>();
+    memset(m_headingSums, 0, sizeof(accumulator) * 8);
+    m_headingSums[0].size = 5 * SAMPLES_PER_SECOND; 
+    m_headingSums[1].size = 10 * SAMPLES_PER_SECOND; 
+    m_headingSums[2].size = 20 * SAMPLES_PER_SECOND; 
+    m_headingSums[3].size = 40 * SAMPLES_PER_SECOND; 
+    m_headingSums[4].size = 80 * SAMPLES_PER_SECOND;
+    m_headingSums[5].size = 160 * SAMPLES_PER_SECOND; 
+    m_headingSums[6].size = 320 * SAMPLES_PER_SECOND; 
+    m_headingSums[7].size = 640 * SAMPLES_PER_SECOND;  // 10 minutes, 40 seconds
 }
 Imu::~Imu(){
     m_pData->clear();
     delete m_pData;
 }
 void Imu::add(ImuData dataPoint){
+    lock();               // This is the longest lock
     m_pData->push_front(dataPoint);
+
+    // Update rolling avereage sums
+    for(int i = 0; i < 8; i++){
+        m_headingSums[i].sum += dataPoint.heading;
+        if(m_headingSums[i].nSamples >= m_headingSums[i].size){
+            m_headingSums[i].sum -= m_pData->end()->heading;
+        }else{
+            m_headingSums[i].nSamples++;
+        }
+    }
+    // TODO: serialize the data to a file on the flash disk and start a new set
+    // For now: resize the list, dropping the oldest data
     if(m_pData->size() > DATA_SIZE){
         m_pData->resize(DATA_SIZE);
     }
-    // Update sums
-
+    unlock();
 }
 int Imu::start(){
     extern int fd;
@@ -50,7 +70,6 @@ int Imu::start(){
     sleep(1);       // Wait for the chip to come back
   
     fd = BNO055_uart_init(B115200);
-    string message = (fd > 0)? "Successfully intialized the UART" : "Error initialing the UART";
     if(fd < 0)  return -1;
 
     bno055.dev_addr = fd;   
@@ -92,12 +111,9 @@ void Imu::imuPoller(Imu* pImu){
 
         // Calculate interval for the next wake up
         chrono::_V2::steady_clock::time_point timePt = 
-            chrono::steady_clock::now() + chrono::milliseconds(SAMPLE_RATE_MS);  
-        
-        mtxData.lock();
-        result = BNO055_read_combined_data(&gyro, &hrp, &accel);
-        mtxData.unlock();
-        
+            chrono::steady_clock::now() + chrono::milliseconds(SAMPLE_RATE_MS);        
+
+        result = BNO055_read_combined_data(&gyro, &hrp, &accel);     
         if(result ==0){
             dataPoint.roll = hrp.r;
             dataPoint.pitch = hrp.p;
@@ -106,55 +122,29 @@ void Imu::imuPoller(Imu* pImu){
             dataPoint.gyroY = gyro.y;
             dataPoint.accX = accel.x;
             dataPoint.accY = accel.y;
-
-            pImu->add(dataPoint);    
-            pImu->updateHeadingSums(dataPoint);
-
+            pImu->add(dataPoint);     
         }
- 
         this_thread::sleep_until(timePt);
     }
 }
-void Imu::getLatestHrp(ImuData *pData){
+void Imu::getData(ImuData *pData){
+    lock();
     ImuData latest = *(m_pData->begin());
     pData->heading=latest.heading;
     pData->roll=latest.roll;
     pData->pitch=latest.pitch;
-}
-void Imu::getLatestGyro(ImuData *pData){
-    ImuData latest = *(m_pData->begin());
     pData->gyroX=latest.gyroX;
     pData->gyroY=latest.gyroY;
-}
-void Imu::getLatestAccel(ImuData *pData){
-    ImuData latest = *(m_pData->begin());
     pData->accX=latest.accX;
     pData->accY=latest.accY;
+    unlock();
 }
 
-int Imu::getAverageHeading(int nAverageIndex){
-    return getHeadingSum(nAverageIndex) / m_pData->size();
-}
-long Imu::getHeadingSum(int index){
-    return m_nHeadingSums[index];
+int Imu::getAverageHeading(int index){
+    lock();
+    return m_headingSums[index].sum / m_headingSums[index].nSamples;
+    unlock();
 }
 int Imu::getHeadingAverageSamples(int index){
-    return m_nHeadingSamples[index];
-}
-void Imu::updateHeadingSums(ImuData dataPoint){
-    memset(m_nHeadingSums, 0, 8 *sizeof(long));
-    int nTotalSamples = m_pData->size();
-    int nCount = 0;
-    for(int i = 0; i < 8; i++){
-        int nSamples = m_nHeadingSamples[i];
-        for(int j = 0; j < nSamples; j++){
-            mtxData.lock();
-            m_nHeadingSums[i] += dataPoint.heading;
-            mtxData.unlock();
-            nCount++;
-            if(nCount >= nTotalSamples) break;
-        }
-        if(nCount >= nTotalSamples) break;
-        if(i < 7) m_nHeadingSums[i + 1] =m_nHeadingSums[i];
-    }
+    return m_headingSums[index].nSamples;   // Read only, does not need mutex lock
 }
